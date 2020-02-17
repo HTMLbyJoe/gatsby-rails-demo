a707dbcbc1ff524cfc4bc594f475cad284e6b67e
Joe Anzalone <joe@shmit.com>
2020-02-17 13:13:18 -0500

    Implement a simple example Apollo query
    With inspiration from https://github.com/jlengstorf/gatsby-with-apollo

---
+++ clients/admin/src/apollo/client.js
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-boost';
+
+export const client = new ApolloClient({
+  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
+});
---
+++ clients/admin/src/pages/test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from "gatsby"
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+// This query is executed at run time by Apollo.
+const APOLLO_QUERY = gql`
+  {
+    meme(where: { id: "cjke2xlf9nhd90953khilyzja" }) {
+      photo {
+        url(
+          transformation: {
+            image: { resize: { width: 600, height: 600, fit: crop } }
+          }
+        )
+      }
+    }
+  }
+`;
+
+const TestPage = () => {
+  const { loading, error, data } = useQuery(APOLLO_QUERY);
+
+  return (
+    <Layout>
+      <SEO title="Test Page" />
+      <p>Stolen from <a href="https://github.com/jlengstorf/gatsby-with-apollo">https://github.com/jlengstorf/gatsby-with-apollo</a></p>
+      <p>This loads at runtime.</p>
+      <div>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: ${error.message}</p>}
+        {data && data.meme && data.meme.photo && (
+          <img
+            src={data.meme.photo.url}
+            alt=""
+            style={{ maxWidth: 300 }}
+          />
+        )}
+      </div>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
+
+export default TestPage
