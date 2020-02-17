1df9a772736b6dce891820eff28008d27f9ce730
Joe Anzalone <joe@shmit.com>
2020-02-17 11:41:36 -0500

    Configure Clearance to be a bit more secure

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,8 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.allow_sign_up = false
+  config.httponly = true
+  config.mailer_sender = 'reply@example.com'
+  config.redirect_url = '/'
   config.rotate_csrf_on_sign_in = true
+  config.secure_cookie = true
 end
