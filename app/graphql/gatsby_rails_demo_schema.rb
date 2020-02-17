0609e6971842ef54c6994de27ad4d4f7b42bfddc
Joe Anzalone <joe@shmit.com>
2020-02-17 12:07:56 -0500

    Fix GraphQL error
    Without this line, this is the error I was getting:
    
        Can't use `GraphQL::Execution::Interpreter` without `GraphQL::Analysis::AST`, please add this plugin to your schema:\n\n    use GraphQL::Analysis::AST\n\nFor information about the new analysis engine: https://graphql-ruby.org/queries/ast_analysis.html\n
    
    (No idea why the schema doesn't come with this line by default)

--- app/graphql/gatsby_rails_demo_schema.rb
+++ app/graphql/gatsby_rails_demo_schema.rb
@@ -5,6 +5,8 @@ class GatsbyRailsDemoSchema < GraphQL::Schema
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
 
+  use GraphQL::Analysis::AST
+
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
 end
