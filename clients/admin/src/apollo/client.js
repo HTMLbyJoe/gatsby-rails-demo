a0310861f88e9c9df9deb1729f7bbac08d0d2637
Joe Anzalone <joe@shmit.com>
2020-05-02 13:03:54 -0400

    Upgrade to Apollo Client 3.0 beta

--- clients/admin/src/apollo/client.js
+++ clients/admin/src/apollo/client.js
@@ -1,5 +1,10 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import fetch from 'node-fetch';
 
 export const client = new ApolloClient({
-  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
+  cache: new InMemoryCache(),
+  link: createHttpLink({
+    fetch,
+    uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
+  }),
 });
--- clients/admin/src/pages/test.js
+++ clients/admin/src/pages/test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "gatsby"
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
