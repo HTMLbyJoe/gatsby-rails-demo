a6373bdac9a56b68ffb67841e1d3e10df47d12d3
Joe Anzalone <joe@shmit.com>
2020-05-05 09:16:18 -0400

    Add error logging to frontend deploy script

--- clients/admin/deploy.js
+++ clients/admin/deploy.js
@@ -1,4 +1,4 @@
 const ghpages = require('gh-pages');
 require('./gatsby-config');
 
-ghpages.publish('public', {repo: process.env.GH_PAGES_REPO});
+ghpages.publish('public', {repo: process.env.GH_PAGES_REPO}, (err) => console.error(err));
