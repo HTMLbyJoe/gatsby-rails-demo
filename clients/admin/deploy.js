2b9d496620fd3c35cd597529cee517643db00955
Joe Anzalone <joe@shmit.com>
2020-05-05 11:01:58 -0400

    Use SSH keypair from env variables before deploy
    This way we can generate a keypair, set the public key as a deploy key in the GitHub repo we're using with GitHub Pages, and put the keypair into environment variables on Heroku

--- clients/admin/deploy.js
+++ clients/admin/deploy.js
@@ -1,4 +1,24 @@
+const fs = require('fs');
 const ghpages = require('gh-pages');
 require('./gatsby-config');
+
+const homeDir = require('os').homedir();
+const keyDir = `${homeDir}/.ssh`;
+const publicKeyFile = `${keyDir}/id_rsa.pub`;
+const privateKeyFile = `${keyDir}/id_rsa`;
+
+const publicKey = process.env.DEPLOY_GH_PAGES_PUBLIC_KEY;
+const privateKey = process.env.DEPLOY_GH_PAGES_PRIVATE_KEY;
+
+if (
+  !fs.existsSync(publicKeyFile) &&
+  !fs.existsSync(privateKeyFile) &&
+  publicKey &&
+  privateKey
+) {
+  fs.mkdirSync(keyDir, { recursive: true });
+  fs.writeFileSync(publicKeyFile, publicKey);
+  fs.writeFileSync(privateKeyFile, privateKey);
+}
 
 ghpages.publish('public', {repo: process.env.GH_PAGES_REPO}, (err) => console.error(err));
