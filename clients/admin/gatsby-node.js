0a32f4dc1bc40df126901bd79b70e746856c43e7
Joe Anzalone <joe@shmit.com>
2020-05-05 20:31:40 -0400

    Allow API_URL to be used in the browser
    This was only working when API_URL came from a .env file because by default, any OS-level environment variables are required to start with `GATSBY_`.
    
    Instead of adding this whitelist, we could just prefix all environment variables to be used in the browser with `GATSBY_`, but having this whitelist option may come in handy.

--- clients/admin/gatsby-node.js
+++ clients/admin/gatsby-node.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  // https://stackoverflow.com/a/60191051
+  const config = getConfig();
+
+  const allowedEnvironmentVariables = [
+    'API_URL',
+  ];
+
+  // Allow whitelisted environment variables to be used in the browser
+  const definePlugin = config.plugins.find(p => p.definitions);
+  for (const [key, value] of Object.entries(process.env)) {
+    if (allowedEnvironmentVariables.includes(key)) {
+      definePlugin.definitions[`process.env.${key}`] = JSON.stringify(value);
+    }
+  }
+
+  actions.replaceWebpackConfig(config);
+};
