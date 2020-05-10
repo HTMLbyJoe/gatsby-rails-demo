d413e09f7d650915924ab2b37d23d2504e9d8198
Joe Anzalone <joe@shmit.com>
2020-05-10 12:44:52 -0400

    Add error handling to frontend of login page
    And redirect to homepage after successful login

--- clients/admin/src/pages/login.js
+++ clients/admin/src/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import Layout from "../components/layout"
@@ -16,8 +16,7 @@ const LOGIN_MUTATION = gql`
 `;
 
 const LoginPage = () => {
-  const [login, { data }] = useMutation(LOGIN_MUTATION);
-  console.log(data);
+  const [login, { data: {login: loginData} = {}, loading }] = useMutation(LOGIN_MUTATION);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -27,13 +26,21 @@ const LoginPage = () => {
     login({ variables: { email, password } });
   };
 
+  if (loginData?.success) {
+    navigate('/');
+    return null;
+  }
+
   return (
   <Layout>
     <SEO title="Login Page" />
+
+    {loginData?.errors.map((error, index) => <div key={index}>{error}</div>)}
+
     <form onSubmit={onSubmit}>
-      <input placeholder="Email" name="email" />
-      <input placeholder="Password" type="password" name="password" />
-      <button type="submit">Go</button>
+      <input placeholder="Email" name="email" disabled={loading} />
+      <input placeholder="Password" type="password" name="password" disabled={loading} />
+      <button type="submit" disabled={loading}>Go</button>
     </form>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
